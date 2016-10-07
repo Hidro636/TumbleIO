9fdb88cefbe98b747ab9d3908613c4fb53f66d00
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-06 17:37:29 -0700

    fixed some boilerplate

--- client/main.js
+++ client/main.js
@@ -2,5 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../imports/ui/tumbleform.html';
 
-
+import '../imports/ui/tumbleform.js';
