1fde27691ac8e767764f85ef2b0ca0202aa6eab1
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-08 14:15:48 -0700

    UI mostly done, starting work on model

--- client/main.js
+++ client/main.js
@@ -1,5 +1,4 @@
 import {Template} from 'meteor/templating';
-import {ReactiveVar} from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/tumbleform.html';
@@ -10,4 +9,11 @@ import '../imports/ui/tumbleform.js';
 Meteor.startup(function () {
     Session.setDefault('transactionFee', 0.0001);
     Session.setDefault('tumbles', 5);
+    Session.setDefault('totalFee', (.0001 * 5) + (.001));
+});
+
+Template.modals.helpers({
+   minimumAmount() {
+       return Number(Session.get('totalFee')) + 0.0001;
+   }
 });
--- imports/ui/tumbleform.js
+++ imports/ui/tumbleform.js
@@ -2,26 +2,28 @@ import {Template} from 'meteor/templating';
 import {Session} from 'meteor/session';
 
 Template.tumbleForm.events({
-    'keyup #toAddressTextBox'(event) {
+    'keyup #toAddressTextBox, click #toAddressTextBox'(event) {
         if (event.target.value.length != 34) {
             $('#toAddressFormGroup').addClass("has-error");
+            $('#toAddressFormGroup').removeClass('hasSuccess');
             $('#tumbleButton').addClass('disabled');
         } else {
             $('#toAddressFormGroup').removeClass('has-error');
             $('#tumbleButton').removeClass('disabled');
+            $('#toAddressFormGroup').addClass('has-success');
         }
     },
     'click #tumbleButton'(event) {
         if ($("#tumbleButton").hasClass('disabled')) return;
-
-
-        console.log(Number($('#defaultTransactionFeeTextBox').val()) * Number($('#tumbleCountTextBox').val()))
+        $('#confirmModal').modal('show');
     },
     'click #defaultTransactionFeeTextBox, keyup #defaultTransactionFeeTextBox'(event) {
         Session.set('transactionFee', event.target.value);
+        Session.set('totalFee', (($('#defaultTransactionFeeTextBox').val() * $('#tumbleCountTextBox').val())) + .001);
     },
     'click #tumbleCountTextBox, keyup #tumbleCountTextBox'(event) {
         Session.set('tumbles', event.target.value);
+        Session.set('totalFee', (($('#defaultTransactionFeeTextBox').val() * $('#tumbleCountTextBox').val())) + .001);
     }
 });
 
