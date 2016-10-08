f5ddd61c7f2e6aaf8b03fa34686aec0466732692
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-08 13:23:57 -0700

    UI work

--- client/main.js
+++ client/main.js
@@ -1,7 +1,13 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/tumbleform.html';
 
 import '../imports/ui/tumbleform.js';
+
+
+Meteor.startup(function () {
+    Session.setDefault('transactionFee', 0.0001);
+    Session.setDefault('tumbles', 5);
+});
--- imports/ui/tumbleform.js
+++ imports/ui/tumbleform.js
@@ -1,23 +1,32 @@
 import {Template} from 'meteor/templating';
+import {Session} from 'meteor/session';
 
 Template.tumbleForm.events({
     'keyup #toAddressTextBox'(event) {
         if (event.target.value.length != 34) {
             $('#toAddressFormGroup').addClass("has-error");
+            $('#tumbleButton').addClass('disabled');
         } else {
             $('#toAddressFormGroup').removeClass('has-error');
+            $('#tumbleButton').removeClass('disabled');
         }
     },
     'click #tumbleButton'(event) {
+        if ($("#tumbleButton").hasClass('disabled')) return;
+
+
         console.log(Number($('#defaultTransactionFeeTextBox').val()) * Number($('#tumbleCountTextBox').val()))
+    },
+    'click #defaultTransactionFeeTextBox, keyup #defaultTransactionFeeTextBox'(event) {
+        Session.set('transactionFee', event.target.value);
+    },
+    'click #tumbleCountTextBox, keyup #tumbleCountTextBox'(event) {
+        Session.set('tumbles', event.target.value);
     }
 });
 
 Template.tumbleForm.helpers({
     estFee() {
-        return Number($('#defaultTransactionFeeTextBox').val()) * Number($('#tumbleCountTextBox').val()) + " BTC";
-    },
-    transactionFee() {
-        return $('#defaultTransactionFeeTextBox').val();
+        return Math.round((Number(Session.get('transactionFee')) * Number(Session.get('tumbles'))) * 1000000) / 1000000;
     }
 });
